9760a3fe7c93e81b5f3c8d4d8c05a368cb2a0e1d
Thomas Wotoro <didymusthomas6@gmail.com>
2025-10-18 23:34:01 +0300

    Add new program ElectrityBill

---
+++ electricityBill.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define True 1
+#define False 0
+#define success 0
+#define failure -1
+
+int main() {
+    // Main function 
+}
+
+double total_energy(double power, double hours, double days) {
+    // Calculates the total energy consumed by a room-mate
+    // kWh = (Watts × Hours × Days) / 1000
+    double result;
+
+    result = (power * hours * days) / 1000;
+}
+
+double power(double work, double time) {
+    // Calculates the power
+}
