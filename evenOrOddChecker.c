9499eb09ef4b45d689c403943efdb6c9a1c25d50
Thomas Wotoro <didymusthomas6@gmail.com>
2025-10-18 23:21:29 +0300

    Modify the Even or Odd program with while loops

--- evenOrOddChecker.c
+++ evenOrOddChecker.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
-// A simple program that checks for even or odd in a number.
+#include <stdlib.h>
+/* A simple program that checks for even or odd in a number.
+    1. Initialize variables and data types
+    2. Accept user inputs
+    3. Evaluate user inputs using Conditionals.
+    4. Display a friendly
+*/ 
+
 int main() {
     // Main function
     int number;
-    // char result;
-    printf("Enter a number: ");
-    scanf("%d", &number);
+    char input[10];
 
-    if (number % 2 == 0) {
-        printf("The number %d is Even", number);
-    } else {
-        printf("The number %d is Odd", number);
+    while (1) {
+
+        printf("Enter a number: (type 'q' to quit): ");
+        scanf("%s", input);
+
+        // Check if user wants to quit
+        if (input[0] == 'q' || input[0] == 'Q') {
+            printf("Thanks for using this program!");
+            break;
+        }
+
+        // Convert string to integer
+        // scanf("%d", &number);
+        number = atoi(input);
+    
+        // Checking user inputs whether its odd or even
+        if (number % 2 == 0) {
+            printf("The number %d is Even\n", number);
+        } else {
+            printf("The number %d is Odd\n", number);
+        }
     }
+
+    return 0;
 }
