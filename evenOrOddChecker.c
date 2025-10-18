e6b4fd9a2d34a5e987cbc2cd6597eae8b6dd7435
Thomas Wotoro <didymusthomas6@gmail.com>
2025-10-18 21:09:07 +0300

    Add even or odd checker program

--- evenOrOddChecker.c
+++ evenOrOddChecker.c
@@ -3,9 +3,9 @@
 int main() {
     // Main function
     int number;
-    char result;
-
-    scanf('%d', &number);
+    // char result;
+    printf("Enter a number: ");
+    scanf("%d", &number);
 
     if (number % 2 == 0) {
         printf("The number %d is Even", number);
