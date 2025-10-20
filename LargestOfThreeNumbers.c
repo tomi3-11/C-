71bec9f77a7c1c96bd4480b688813ee81b3c5f24
Thomas Wotoro <didymusthomas6@gmail.com>
2025-10-21 00:46:22 +0300

    Add LargestOfThreeNumbers.c

---
+++ LargestOfThreeNumbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Main function fumctionality
+    int numbers[] = {4, 5, 6, 9, 1, 2, 10};
+
+    // Getting the length of the strength
+    int length = sizeof(numbers) / sizeof(numbers[0]);
+
+    // Getting the highest number from the list
+    int max = numbers[0];
+
+    for (int i = 0; i < length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    // print the result
+    printf("The largest number is %d\n", max);
+
+    return 0;
+}
