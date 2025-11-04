4f62b9b7dc59c684d6973b8001faf1b9a295e8d4
Thomas Wotoro <didymusthomas6@gmail.com>
2025-11-04 19:42:29 +0300

    Add SumOfNaturalNumbers

--- SumOfNaturalNumbers.c
+++ SumOfNaturalNumbers.c
@@ -6,5 +6,14 @@ int main() {
     // Main Function.
     int nums[] = {2, 4, 5};
 
+    int length = sizeof(nums) / sizeof(nums[0]);
+
+    int answer = 0;
+
+    for (int i = 0; i < length; i++) {
+        answer += nums[i];
+    }
+    
+    printf("The sum of natural numbers is %d", answer);
     return 0;
 }
