0b6cf5e6ac21c952572ecf44d7ec827045cce7a9
thomas <didymusthomas6@gmail.com>
2025-11-08 19:35:38 +0300

    Add factorial and modify

--- factorial.c
+++ factorial.c
@@ -6,13 +6,14 @@ int fac(int num);
 int main() {
     int n;
     printf("Enter a number: ");
-    scanf("%d", &n);
+    scanf("%d", &n); // prompting the user for a number
 
-    int result = fac(n);
-    printf("The factorial of %d is %d.\n", n, result);
+    int result = fac(n); // invoking the factorial function
+    printf("The factorial of %d is %d.\n", n, result); // Displaying the result
     
 }
 
+// Function that handles the factorial of a number
 int fac(int num) {
     if (num == 0 || num == 1) {
         return 1;
