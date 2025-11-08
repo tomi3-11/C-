4e27f9cf1d9e50c7910159aa990b3dff6cf453ed
thomas <didymusthomas6@gmail.com>
2025-11-08 19:41:52 +0300

    Update SumOfNaturalNumbers by documenting it

--- SumOfNaturalNumbers.c
+++ SumOfNaturalNumbers.c
@@ -3,17 +3,20 @@
 
 // main function
 int main() {
-    // Main Function.
+    // Initializing an array
     int nums[] = {2, 4, 5};
-    
+    // Getting the size of the array
     int length = sizeof(nums) / sizeof(nums[0]);
-
+    // Initializing the answer variable to 0 so as to be added to 
     int answer = 0;
 
+    // Looping through all the contents in the array
     for (int i = 0; i < length; i++) {
+        // adding the individual member to the answer variable
         answer += nums[i];
     }
     
+    // Displaying the answer to the user
     printf("The sum of natural numbers is %d\n", answer);
     return 0;
 }
