2207e5c4835f9ccfee298c35f2a0dca4e76a71ca
thomas <didymusthomas6@gmail.com>
2025-11-09 20:57:05 +0300

    Update code logic

--- primeNumberChecker.c
+++ primeNumberChecker.c
@@ -21,7 +21,7 @@ int main() {
         return 0;
     }
 
-    for (int i = 0; i < sqrt(num) + 1; i ++) {
+    for (int i = 2; i < sqrt(num) + 1; i ++) {
         if (num % i == 0) {
             return 1;
             printf("Number %d is a prime number.\n", num);
