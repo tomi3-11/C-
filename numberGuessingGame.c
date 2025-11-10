44ad864344834758c445a202559bede10ccc49ad
thomas <didymusthomas6@gmail.com>
2025-11-10 18:44:46 +0300

    Update number to change after every trial

--- numberGuessingGame.c
+++ numberGuessingGame.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
 // #define True 1
 // #define False 0
@@ -8,22 +9,21 @@
 int main() {
     // Initializing project variables
     int num, guess;
-    // Bookean intialized to false
-    bool numberFound = true;
-    // Lower number
-    int lowerNumber = 0;
-    // upper number
-    int upperNumber = 100;
-    // Generating number to be guessed
-    guess = rand() % (upperNumber - lowerNumber + 1) + lowerNumber;
 
-    while (numberFound) {
+    // Highest Number
+    int N = 100;
+
+    srand((unsigned int)time(NULL));
+
+    guess = rand() % (N + 1);
+
+    while (true) {
         // Taking user inputs
         printf("Enter a number to guess: ");
         scanf("%d", &num);
 
         if (num == guess) {
-            printf("You guessed the number %d right.", guess);
+            printf("You guessed the number %d right.\n", guess);
             break;
         } else if (num < guess) {
             printf("Too low! try again\n");
@@ -32,17 +32,6 @@ int main() {
         }
 
     } 
-    printf("%d \n", guess);
-
-
-
-
-
-    // for (int i = 0; i < 10; i ++){
-    //     int value = rand() % (upperNumber - lowerNumber + 1) + lowerNumber;
-
-    //     printf("%d \n", value);
-    // }
 
     return 0;
 }
