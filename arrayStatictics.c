bf3caf896fe2745ae1d761c179b69710fb4879f9
thomas <didymusthomas6@gmail.com>
2025-11-11 14:28:53 +0300

    Update and refactor code

--- arrayStatictics.c
+++ arrayStatictics.c
@@ -2,34 +2,37 @@
 #include <stdlib.h>
 
 // Function prototypes
-int sum(int array[]);
+// int sumOf(int array);
 
-int main(void) {
+int main() {
     // Find sum, average, min, and max of array elements.
     int array[] = {2, 5, 6, 8, 1, 9};
 
-    int sum;
-
-    sum = sumOf(array);
-    printf("%d", sum);
+    sumOf(array);
+    
 }
 
 int sumOf(int array[]) {
     // Calculates the sum of an array
-    int sum = 0;
+    int sum;
 
+    // sum = sumOf(array);
     int length = sizeof(array) / sizeof(array[0]);
 
     for (int i = 0; i < length; i++){
         sum += array[i];
     }
+
+
+
+    printf("The sum of all the numbers is %d\n", sum);
 }
-int sum(int array[]) {
-    // Calculates the average of an array
-}
-int sum(int array[]) {
-    // Calculates the min of an array
-}
-int sum(int array[]) {
-    // Calculates the max of an array
-}
+// int sum(int array[]) {
+//     // Calculates the average of an array
+// }
+// int sum(int array[]) {
+//     // Calculates the min of an array
+// }
+// int sum(int array[]) {
+//     // Calculates the max of an array
+// }
