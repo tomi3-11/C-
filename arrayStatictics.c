3c2c10493fa3acf30ed587599b6d342216eda9a5
thomas <didymusthomas6@gmail.com>
2025-11-11 14:17:20 +0300

    Add array statistics file

---
+++ arrayStatictics.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+    // Find sum, average, min, and max of array elements.
+    int array[] = {2, 5, 6, 8, 1, 9};
+}
+
+int sum(int array[]) {
+    // Calculates the sum of an array
+}
+int sum(int array[]) {
+    // Calculates the average of an array
+}
+int sum(int array[]) {
+    // Calculates the min of an array
+}
+int sum(int array[]) {
+    // Calculates the max of an array
+}
