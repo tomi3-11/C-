c57a7129f63c260c31bd71f75ba40d038b22251f
thomas <didymusthomas6@gmail.com>
2025-11-11 14:36:06 +0300

    Update with working sum function

--- arrayStatictics.c
+++ arrayStatictics.c
@@ -2,31 +2,34 @@
 #include <stdlib.h>
 
 // Function prototypes
-// int sumOf(int array);
+int sumOf(int array[], int size);
 
 int main() {
     // Find sum, average, min, and max of array elements.
     int array[] = {2, 5, 6, 8, 1, 9};
+    int size = sizeof(array) / sizeof(array[0]);
 
-    sumOf(array);
+
+    // Handling Sum.
+    printf("The sum of all the numbers is %d\n", sumOf(array, size));
+    return 0;
     
 }
 
-int sumOf(int array[]) {
+int sumOf(int array[], int size) {
     // Calculates the sum of an array
-    int sum;
-
-    // sum = sumOf(array);
-    int length = sizeof(array) / sizeof(array[0]);
-
-    for (int i = 0; i < length; i++){
+    int sum = 0;
+    for (int i = 0; i < size; i++){
         sum += array[i];
     }
-
-
-
-    printf("The sum of all the numbers is %d\n", sum);
+    return sum;
 }
+
+
+
+
+
+
 // int sum(int array[]) {
 //     // Calculates the average of an array
 // }
