a0a588fcc5f5b5765a693f20b3788bece9697ec5
thomas <didymusthomas6@gmail.com>
2025-11-12 20:49:32 +0300

    Update logic with clear documentation.

--- reverseArray.c
+++ reverseArray.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <string.h>
 
 int main() {
     // Reverse an Array | Practice array traversal and swapping.
-    char name[20];
+    // Initializing required variables.
+    char name[100], rev[100];
+    int i, size;
     
+    // getting user inputs
     printf("Enter your name: ");
     scanf("%s", name);
 
-    printf("Hello %s\n", name);
+    size = strlen(name); // getting then length of the string.
+    // printf("The size of the string is %d\n", size);
+
+    for (i = 0; i < size; i++) {
+        rev[i] = name[size - i - 1];  
+    }
+    rev[size] = '\0';
+
+    printf("Reversed as: %s\n", rev);
+
+    return 0;
 }
+
+
+// char reverse(char string[20]) {
+//     // Performs string reversal.
+//     char words[] = {};
+// }
