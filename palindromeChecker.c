16634b2ece1fb9aa493deac9a5e629b20ff51ee5
thomas <didymusthomas6@gmail.com>
2025-11-15 19:56:37 +0300

    Add palindromeChecker.c file and update .gitignore

---
+++ palindromeChecker.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+// function prototypes
+char reverse(char rev[], char word[]);
+
+
+int main() {
+    // Palindrome Checker | Check if a string or number reads the same backward.
+    char word[200], rev[200];
+
+    printf("Enter a word: ");
+    scanf("%s", word);
+
+    reverse(rev, word);
+
+    return 0;
+}
+
+char reverse(char rev[], char word[]) {
+    // getting the length of the word
+    int size = strlen(word);
+
+    // looping through the word to reverse it
+    for (int i = 0; i < size; i++) {
+        rev[i] = word[size - i - 1];
+    }
+    rev[size] = '\0';
+
+    printf("%s\n", rev);
+}
