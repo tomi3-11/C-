52461bd90f0fd95c3d4cff38170a237728566ac1
thomas <didymusthomas6@gmail.com>
2025-11-16 19:57:32 +0300

    Add simpleVotingSystem.c

---
+++ simpleVotingSystem.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+    // Simple Voting System | Count votes using arrays. 
+    char votes[] = {};
+
+    char user_input[10];
+
+    printf("Enter vote: ");
+    printf("Yes or No");
+    scanf("%s", user_input);
+
+
+}
