e536effb18e105037c6a366309f763fd6cb0e839
tom <didymusthomas6@gmail.com>
2025-11-30 22:36:17 +0300

    Add file for simpleBankingsystem

--- simpleBankingSystem/simpleBankingSystem.c
+++ simpleBankingSystem/simpleBankingSystem.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
+struct bankAccount {
+    char fname[20];
+    char lname[20];
+    int accountNumber;
+    int accountBalance;
+};
+
 
 int main() {
     // | 20   | Simple Banking System (CLI) | Deposit, withdraw, check balance (using functions and structures). |
 
-    struct bankAccount {
-        char fname[20];
-        char lname[20];
-        int accountNumber;
-        int accountBalance;
-    }
+    struct bankAccount account;
 
-    Struct bankAccount account;
-
-    account.fname = "Tom";
-    account.lname = "Joe";
+    strcpy(account.fname, "Tom");
+    strcpy(account.lname, "Joe");
     account.accountNumber = 12345;
     account.accountBalance = 20000;
+
+    printf("The first name of the account holder is %s", account.fname);
+    printf("The last name of the account holder is %s", account.lname);
+    printf("The account number for %s %s is %d", account.fname, account.lname, account.accountNumber);
+    printf("The account balance for %s %s is %d", account.fname, account.lname, account.accountBalance);
+
+
 
 }
