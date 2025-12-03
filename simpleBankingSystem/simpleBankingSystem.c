e5b48f0695fa62959192f7d55cede822a536de57
tom <didymusthomas6@gmail.com>
2025-12-03 10:11:17 +0300

    Update the simplebanking system with structures

--- simpleBankingSystem/simpleBankingSystem.c
+++ simpleBankingSystem/simpleBankingSystem.c
@@ -1,28 +1,81 @@
 #include <stdio.h>
 #include <string.h>
 
-struct bankAccount {
+struct BankAccount {
     char fname[20];
     char lname[20];
     int accountNumber;
     int accountBalance;
 };
 
+struct Bank {
+    struct BankAccount process_account;
+};
+
+int createAccount(char fName[20], char lName[20]) {
+    // Creating user account
+    // Creating a struct variable.
+    struct BankAccount create_account;
+
+    // Creating user details for the new account
+    strcpy(create_account.fname, fName);
+    strcpy(create_account.lname, lName);
+    create_account.accountNumber = 12345;
+
+    printf("The first name of the account holder is %s\n", create_account.fname);
+    printf("The last name of the account holder is %s\n", create_account.lname);
+    printf("The account number for %s %s is %d\n", create_account.fname, create_account.lname, create_account.accountNumber);
+    //printf("The account balance for %s %s is %d\n", account.fname, account.lname, account.accountBalance);
+
+    return 0;
+}
+
 
 int main() {
     // | 20   | Simple Banking System (CLI) | Deposit, withdraw, check balance (using functions and structures). |
+    /*
+    In bankAccount:
+        the user creates an account with:(Basic details for Now)
+            first Name
+            Last Name
+        then the program create an account Number.
+        Functions to be used here:
+        {
+            "Create Account",
+            "Display account details",
+            "Print my details"
+        }
 
-    struct bankAccount account;
+    In Bank:
+        the Bank automates the user processes:
+            Allows user to transact from one account to another
+            Allows users to deposit.
+            Allows users to withdraw.
+            Allows users to check balance.
+        Functions to be used here:
+        {
+            "Transaction",
+            "Deposit",
+            "Withdraw",
+            "Check_balance"
+        }
+    */
 
-    strcpy(account.fname, "Tom");
-    strcpy(account.lname, "Joe");
-    account.accountNumber = 12345;
-    account.accountBalance = 20000;
+    struct BankAccount account;
 
-    printf("The first name of the account holder is %s\n", account.fname);
-    printf("The last name of the account holder is %s\n", account.lname);
-    printf("The account number for %s %s is %d\n", account.fname, account.lname, account.accountNumber);
-    printf("The account balance for %s %s is %d\n", account.fname, account.lname, account.accountBalance);
+    // Getting user details 
+    printf("Enter first Name: ");
+    scanf("%s", account.fname);
+    printf("Enter last Name: ");
+    scanf("%s", account.lname);
+
+
+    createAccount(account.fname, account.lname);
+    //strcpy(account.fname, "Tom");
+    //strcpy(account.lname, "Joe");
+    //account.accountNumber = 12345;
+    //account.accountBalance = 20000;
+
 
 
 
