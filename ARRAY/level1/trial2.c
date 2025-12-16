4e89dce203fbb3b29f280a1a7b8860d9c1518eb1
thomas <didymusthomas6@gmail.com>
2025-12-16 23:51:48 +0300

    Add trial2

---
+++ ARRAY/level1/trial2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    // Read 5 integer into an array and print them in the same order.
+    int nums[5];
+
+    // Add numbers
+    nums[0] = 1;
+    nums[1] = 2;
+    nums[2] = 3;
+    nums[3] = 4;
+    nums[4] = 5;
+
+    // Printing all the numbers
+    printf("Array 1: %d\n", nums[0]);
+    printf("Array 2: %d\n", nums[1]);
+    printf("Array 3: %d\n", nums[2]);
+    printf("Array 4: %d\n", nums[3]);
+    printf("Array 5: %d\n", nums[4]); 
+
+}
