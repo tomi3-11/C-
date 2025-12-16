069316a1a3d3627070ff0a4ad0ca8c45d82b14be
thomas <didymusthomas6@gmail.com>
2025-12-16 23:39:30 +0300

    Add array folder to handle array learning

---
+++ ARRAY/level1/trial1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declare an integer array of size 10 and initialize it with zeros
+    int arr[10] = {0, 0, 0, 0};
+
+    printf("Array 1: %d\n", arr[0]);
+}
