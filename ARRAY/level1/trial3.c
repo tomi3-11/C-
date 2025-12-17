07323ea35501dcc36520e4719e6a718a17a7b950
thomas <didymusthomas6@gmail.com>
2025-12-17 21:54:11 +0300

    Update a fixed bug for trial 3

--- ARRAY/level1/trial3.c
+++ ARRAY/level1/trial3.c
@@ -5,7 +5,7 @@ int main()
     // Print all elements of an array using
     // A. For loop
     
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7};
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
     
     int size = sizeof(arr)/sizeof(arr[0]);
     for (int i = 0; i < size; i++) {
