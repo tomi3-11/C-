df158dbca6b021a298899c450633b84e4e05d174
thomas <didymusthomas6@gmail.com>
2025-12-17 22:19:40 +0300

    Add trial 5 with size in bytes for char  array

--- ARRAY/level1/trial5.c
+++ ARRAY/level1/trial5.c
@@ -5,6 +5,13 @@ int main() {
     // 1. An integer array of size 10
     int nums[10];
     int size = sizeof(nums);
-    printf("Size in bytes for integer array: %d\n", size);
+    printf("Size in bytes for integer array: %d bytes.\n", size);
+
+    // 2. A character array of size 10
+    char letters[10];
+    int sizeLetters = sizeof(letters);
+    printf("Size in bytes for character array: %d bytes.\n", sizeLetters);
+
+    return 0;
 
 }
