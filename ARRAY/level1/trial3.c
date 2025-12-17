681c85e901c72c4a4cf7c935c77662efe5398b53
thomas <didymusthomas6@gmail.com>
2025-12-17 21:58:38 +0300

    Update with both for and while loop for trial 3

--- ARRAY/level1/trial3.c
+++ ARRAY/level1/trial3.c
@@ -4,12 +4,22 @@ int main()
 {
     // Print all elements of an array using
     // A. For loop
-    
+   
+    printf("Using a for Loop: \n");	
     int arr[] = {1, 2, 3, 4, 5, 6, 7};
     
     int size = sizeof(arr)/sizeof(arr[0]);
     for (int i = 0; i < size; i++) {
         printf("%d\n", arr[i]);
+    }
+
+    // B. While loop
+    printf("Using a while Loop: \n");
+
+    int count = 0;
+    while (count < size) {
+        printf("%d\n", arr[count]);
+	count ++;
     }
 
     return 0;    
