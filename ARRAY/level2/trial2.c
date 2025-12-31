c981b86b60e3a5ddea01ac3a42027574eb33920e
thomas <didymusthomas6@gmail.com>
2025-12-31 18:46:13 +0300

    Add trial2 sum & average

--- ARRAY/level2/trial2.c
+++ ARRAY/level2/trial2.c
@@ -3,6 +3,25 @@
 int main()
 {
     // Getting the average of and array
+    int arr[] = {2, 5, 4, 7, 8, 9};
 
+    int sum = 0;
+
+    // Getting the length of the array
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < size; i++) {
+	// adding all the numbers to the sum
+	sum += arr[i];
+    }
+
+    // the avarage is the sum / size
+    int average = sum / size;
+
+    // Displaying to the user
+    printf("The sum of the array is %d.\n", sum);
+    printf("The average is %d.\n", average);
+
+    return 0;
 
 }
