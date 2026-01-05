824c0a3d91a7f348473e3704663a456bcf08cffa
thomas <didymusthomas6@gmail.com>
2026-01-05 20:17:10 +0300

    Update and add trial4 for largest number in an array

---
+++ ARRAY/level2/trial4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    // find the largest element in an array
+    int arr[] = {2, 5, 6, 3, 1, 9};
+
+    // Maximum value
+    int max = 0;
+
+    // size of the array
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < size; i++){
+	if (arr[i] > max){
+	    max = arr[i];
+	}
+
+    }
+    printf("The larget element is %d.\n", max);
+
+    return 0;
+
+
+}
