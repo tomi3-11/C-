6cf17d4abb3edde3f1009d470aef2103a13f8fff
tom <didymusthomas6@gmail.com>
2026-01-14 00:42:28 +0300

    Add fibonacci

--- LogicBuilding/fibonacci/fibonacci.c
+++ LogicBuilding/fibonacci/fibonacci.c
@@ -1,15 +1,15 @@
-#include <stdio.c>
+#include <stdio.h>
 
 int main() {
     // Finding the first 20 fibonacci numbers
     int prev1 = 0;
     int prev2 = 1;
     
-    printf("%d", prev0);
     printf("%d", prev1);
+    printf("%d", prev2);
 
     for (int i = 0; i < 19; i++){
-	int newFibo = prev0 + prev1;
+	int newFibo = prev1 + prev2;
 	printf("%d", newFibo);
 
 	prev2 = prev1;
