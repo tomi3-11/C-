808e98d378b567ebd5cc624412eb65c77f546aae
tom <didymusthomas6@gmail.com>
2026-01-14 00:43:37 +0300

    Update fibonacci for bug fixes

--- LogicBuilding/fibonacci/fibonacci.c
+++ LogicBuilding/fibonacci/fibonacci.c
@@ -5,12 +5,12 @@ int main() {
     int prev1 = 0;
     int prev2 = 1;
     
-    printf("%d", prev1);
-    printf("%d", prev2);
+    printf("%d\n", prev1);
+    printf("%d\n", prev2);
 
     for (int i = 0; i < 19; i++){
 	int newFibo = prev1 + prev2;
-	printf("%d", newFibo);
+	printf("%d\n", newFibo);
 
 	prev2 = prev1;
 	prev1 = newFibo;
