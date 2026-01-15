0f1cc9c9324aa541c14a96fb814d04f93c97a5be
tom <didymusthomas6@gmail.com>
2026-01-15 22:26:55 +0300

    Add multiplication table

---
+++ LogicBuilding/multiplicationTable/multiplicationTable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // A multiplication table for a given number
+    int num, i, result;
+
+    printf("Enter a number for the table: ");
+    scanf("%d", &num);
+
+    for (i = 0; i < 12; i++){
+	result = num * i;
+	printf("%d x %d = %d\n", num, i, result);
+    }
+
+    return 0;
+
+}
