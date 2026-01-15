07959eecd654be329d172a10d5bb73bb93810eb4
tom <didymusthomas6@gmail.com>
2026-01-15 23:42:36 +0300

    Update the reverse array

--- ARRAY/level2/trial7.c
+++ ARRAY/level2/trial7.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 
+// function prototypes
+int reverse(int arr[], int rev[]);
+
 int main() {
     // Reverse an array
     int arr[] = {2, 5, 7, 1, 4, 9};
+    
+    int rev[];
 
     // array size
+    //int size = sizeof(arr) / sizeof(arr[0]);
+
+    reverse(arr, rev);
+    
+
+    return 0;
+}
+
+int reverse(int arr[], int rev[]) {
     int size = sizeof(arr) / sizeof(arr[0]);
 
+    for (int i = 0; i < size; i++) {
+	rev[i] = arr[size - i - 1];
+    }
+    rev[size] = '\0';
 
+    printf("Reversed as: %d\n", rev);
 }
+
+
