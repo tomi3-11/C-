8c46c6b2ada0757d9cbaab8a316183940047cc90
tom <didymusthomas6@gmail.com>
2026-01-23 21:51:00 +0300

    Add lab for data types sizes

--- Basics/C_and_memory_foundation/dataTypes-C/test.c
+++ Basics/C_and_memory_foundation/dataTypes-C/test.c
@@ -10,6 +10,15 @@ int main(){
 
 	// Getting their sizes
 	int integer_size = sizeof(number);
+	int float_size = sizeof(pi);
+	int char_size = sizeof(letter);
+	int double_size = sizeof(balance);
+	
+	// Displaying the sizes of the data types
+	printf("The size of an integer is %d. bytes.\n", integer_size);
+	printf("The size of an float is %d. bytes.\n", float_size);
+	printf("The size of an char is %d. bytes.\n", char_size);
+	printf("The size of an double is %d. bytes.\n", double_size);
 
-	printf("The size of an integer is %d.\n", integer_size);
+	return 0;
 }
