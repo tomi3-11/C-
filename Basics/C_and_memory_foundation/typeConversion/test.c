9d4fe14981c3a66440c1e1015454e1ed0f43c78f
tom <didymusthomas6@gmail.com>
2026-01-24 14:32:06 +0300

    Type conversion for boolean to float

--- Basics/C_and_memory_foundation/typeConversion/test.c
+++ Basics/C_and_memory_foundation/typeConversion/test.c
@@ -3,11 +3,17 @@
 
 int main() {
 	// Type conversion in action using Assignment
+	// Initialization of x
 	bool x = true;
 
+	// bool to integer
 	int y = x;
+
+	// bool to float
+	float z = x;
 
 	printf("Display x %d.\n", x);
 	printf("Display y %d.\n", y);
+	printf("Display z %f.\n", z);
 
 }
