ac812fb780beba5880ed49c194884baf8b98cfe2
tom <didymusthomas6@gmail.com>
2026-01-27 14:50:58 +0300

    Add test program for variables

---
+++ Basics/C_and_memory_foundation/variables/variablesTest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	// Initialization and declaration differently
+	int number;
+	number = 5;
+
+	// Initialization and declaration at the same time
+	int letter = 'a'; 
+	
+	// Displaying the variables
+	printf("Integer: %d.\n", number);
+	printf("Character: %c.\n", letter);
+
+
+}
