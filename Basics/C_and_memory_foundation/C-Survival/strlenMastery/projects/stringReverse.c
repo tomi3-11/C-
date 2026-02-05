592ec660ca584410827e92f304842ca82d003c39
thomas <didymusthomas6@gmail.com>
2026-02-05 15:22:41 +0300

    Add string reverse

--- Basics/C_and_memory_foundation/C-Survival/strlenMastery/projects/stringReverse.c
+++ Basics/C_and_memory_foundation/C-Survival/strlenMastery/projects/stringReverse.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
+void rev(char* s){
+    // initialize l and r pointers
+    int l = 0;
+    int r = strlen(s) - 1;
+    char t;
+
+    // Swap characters till l and r meet
+    while (l < r) {
+            // Swap characters
+            t = s[l];
+            s[l] = s[r];
+            s[r] = t;
+
+            l++;
+            r--;
+    }
+}
+
 int main(){
+        char s[100] = "Hello";
 
+        // Reversing
+        rev(s);
 
+        printf("%s\n", s);
+        return 0;
 
 }
