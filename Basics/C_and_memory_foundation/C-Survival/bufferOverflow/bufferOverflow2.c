251f02b67425bbfe7a36859dfb19e42fa3eefd4d
thomas <didymusthomas6@gmail.com>
2026-02-08 20:30:05 +0300

    Intentional buffer overflow: Errors expected

---
+++ Basics/C_and_memory_foundation/C-Survival/bufferOverflow/bufferOverflow2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+        char buffer[8];
+        int x = 42;
+
+        strcpy(buffer, "AAAAAAAAAAAA");
+
+        printf("x = %d\n", x);
+
+        return 0;
+}
