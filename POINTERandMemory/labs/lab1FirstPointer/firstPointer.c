ee04451f2acc1fd3a1cc9f5f7d22fc382bda7520
thomas <didymusthomas6@gmail.com>
2026-02-10 23:42:54 +0300

    Add first pointer lab

--- POINTERandMemory/labs/lab1FirstPointer/firstPointer.c
+++ POINTERandMemory/labs/lab1FirstPointer/firstPointer.c
@@ -5,8 +5,8 @@ int main(){
         int x = 10;
         int *p = &x;
 
-        printf("%d\n", x);
-        printf("%d\n", *p);
-        printf("%p\n", (void*)p);
+        printf("x: %d\n", x);
+        printf("Dereferencing x: %d\n", *p);
+        printf("Pointer of x: %p\n", (void*)p);
 
 }
