96669a7981971f3d03985a9704dae6c0aada7e2f
thomas <didymusthomas6@gmail.com>
2026-02-17 15:13:37 +0300

    Update bug for buffer overflow: (still buggy tho)

--- Basics/C_and_memory_foundation/C-Survival/strlenMastery/projects/passwordStrength.c
+++ Basics/C_and_memory_foundation/C-Survival/strlenMastery/projects/passwordStrength.c
@@ -11,18 +11,18 @@ int StrLen(char *word){
 
 int main() {
     // A program the checks for password strength.
-    char word[20];
+    char word[50];
     printf("Enter your password: ");
     scanf("%s", word);
     
     if (StrLen(word) < 8){
-        printf("Your password is week and short.");
+        printf("Your password is week and short.\n");
     } else if (8 <= StrLen(word) <= 15) {
-        printf("Your password is medium but needs strengthening for better security.");
+        printf("Your password is medium but needs strengthening for better security.\n");
     } else if (StrLen(word) > 15) {
-        printf("Mate your password is strong enough, Aura");
+        printf("Mate your password is strong enough, Aura\n");
     } else {
-        printf("You better enter a strong password.");
+        printf("You better enter a strong password.\n");
     }
     
     // int size = StrLen(word);
